b61f83941fd4d345a1a02faf070663a14bd3c658
leeevelyn <EvelynLee1295@gmail.com>
2026-05-07 05:42:36 -0400

    trying to fix layout

--- Final_Project/gsap.js
+++ Final_Project/gsap.js
@@ -1,29 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
   gsap.registerPlugin(ScrollTrigger);
 
-  const horizontalSections = gsap.utils.toArray('section.horizontal');
+  const horizontalSections = gsap.utils.toArray('.horizontal');
 
   horizontalSections.forEach((sec) => {
     const animWrap = sec.querySelector('.animation-wrap');
     const isRight = animWrap.classList.contains('to-right');
 
-    const getTravelDistance = () => animWrap.scrollWidth - window.innerWidth;
+    const getTravelDistance = () =>
+      animWrap.scrollWidth - window.innerWidth;
 
-    gsap.fromTo(animWrap,
-      { x: isRight ? 0 : () => -getTravelDistance() },
+    gsap.fromTo(
+      animWrap,
       {
-        x: isRight ? () => -getTravelDistance() : 0,
-        ease: 'circ',
+        x: isRight ? 0 : -getTravelDistance()
+      },
+      {
+        x: isRight ? -getTravelDistance() : 0,
+        ease: "none",
         scrollTrigger: {
           trigger: sec,
-          start: 'top top',
-          end: () => '+=' + getTravelDistance(),
-          pin: true,
-          pinSpacing: true,
+          start: "top top",
+          end: () => `+=${getTravelDistance()}`,
           scrub: 1,
-          invalidateOnRefresh: true,
+          pin: true,
+          invalidateOnRefresh: true
         }
       }
     );
+  });
+
+  window.addEventListener("load", () => {
+    ScrollTrigger.refresh();
   });
 });
